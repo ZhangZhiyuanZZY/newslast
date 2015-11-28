f80e7c5fa6275a878d62d9cc09bc28045f36919b
章芝源 <610925874@qq.com>
2015-11-28 23:35:01 +0800

    首页假数据, 基本设置

---
+++ 网易新闻高级版/网易新闻高级版/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻高级版
+//
+//  Created by 章芝源 on 15/11/28.
+//  Copyright © 2015年 ZZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
